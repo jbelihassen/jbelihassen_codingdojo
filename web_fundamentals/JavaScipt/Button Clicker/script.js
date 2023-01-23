e6501d132d0c84a7a53e3b53f27fde299662621a
John Doe <johndoe@example.com>
2023-01-23 18:39:38 +0100

    Javascript day 06

---
+++ web_fundamentals/JavaScipt/Button Clicker/script.js
@@ -0,0 +1,13 @@
+// function logout
+function logout(id){
+    document.getElementById(id).innerText="logout"
+}
+// function hide
+function hide(id){
+     document.getElementById(id).remove()
+}
+// function aler
+function aler(id){
+     document.getElementById(id)
+     alert("ninja was liked")
+}
---
+++ web_fundamentals/JavaScipt/loop_challenge/loopchallenge.js
@@ -0,0 +1,56 @@
+
+// Print odds 1-20
+
+
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+  }
+
+
+// Decreasing Multiples of 3
+
+for (var i = 100; i => 0; i--) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+  
+  } 
+
+
+//  Print the sequence
+
+
+
+
+for (var i = 4; i > -4 ; i-= 1.5) {
+    console.log(i);
+} 
+
+// 
+
+
+// Sigma
+
+
+var sum=0
+function result(){
+  for (var i = 1; i <= 100 ; i++) {
+   sum+=i     
+  } 
+ }
+result()
+console.log(sum)
+
+
+
+// Factorial
+var sum=1
+function result(){
+  for (var i = 1; i <= 12 ; i++) {
+   sum*=i     
+  } 
+ }
+result()
+console.log(sum)
